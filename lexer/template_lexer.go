56b685c125da6a7663011fb8b79667041d8a0bdf
yetsing <heopro@163.com>
2026-05-21 11:16:10 +0800

    feat: enhance TemplateLexer with support raw block

--- lexer/template_lexer.go
+++ lexer/template_lexer.go
@@ -107,14 +107,61 @@ func (tl *TemplateLexer) getToken(index int) token.Token {
 	if index >= len(tl.tokens) {
 		// 索引超出已读取范围，再次进行读取
 		tk := tl.readToken()
-		for tl.shouldIgnoreToken(tk) {
+		ignored := false
+		for {
 			// 如果是需要忽略的 token ，继续读取下一个
-			tk = tl.readToken()
+			tk, ignored = tl.convertToken(tk)
+			if ignored {
+				tk = tl.readToken()
+				continue
+			}
+			break
 		}
-		tk = tl.convertToken(tk)
 		tl.tokens = append(tl.tokens, tk)
 	}
 	return tl.tokens[index]
+}
+
+func (tl *TemplateLexer) convertToken(tk token.Token) (token.Token, bool) {
+	var err error
+
+	if tk.TypeIn(tl.ignoredTokens...) {
+		return tk, true
+	}
+	if tk.TypeIn(tl.ignoreIfEmpty...) && tk.Literal == "" {
+		return tk, true
+	}
+
+	switch tk.Type {
+	case token.TOKEN_LINESTATEMENT_BEGIN:
+		tk.Type = token.TOKEN_BLOCK_BEGIN
+	case token.TOKEN_LINESTATEMENT_END:
+		tk.Type = token.TOKEN_BLOCK_END
+	case token.TOKEN_RAW_BEGIN, token.TOKEN_RAW_END:
+		return tk, true
+	case token.TOKEN_DATA:
+		tk.Literal = tl.normalizeNewlines(tk.Literal)
+	case token.TOKEN_STRING:
+		if tk.Literal == "" {
+			break
+		}
+		// remove the quotes and unescape the string literal
+		literal := tk.Literal[1 : len(tk.Literal)-1]
+		// Unquote 不支持多行双引号字符串，因此分行 Unquote
+		lines := strings.Split(literal, "\n")
+		for i, line := range lines {
+			lines[i], err = strconv.Unquote(fmt.Sprintf(`"%s"`, line))
+			if err != nil {
+				tk.Type = token.TOKEN_ILLEGAL
+				tk.Literal = fmt.Sprintf("string literal unquote error: %v", err)
+				return tk, false
+			}
+		}
+		// 跟 jinja2 保持一致，字符串中的换行会被转换成配置中的换行序列
+		// 注意：替换的换行是文本本身的换行，而不是字符串字面量中的 \n
+		tk.Literal = strings.Join(lines, tl.config.NewlineSequence)
+	}
+	return tk, false
 }
 
 func (tl *TemplateLexer) readToken() token.Token {
@@ -128,11 +175,16 @@ BEGIN:
 		token.TOKEN_BLOCK_END,
 		token.TOKEN_VARIABLE_BEGIN,
 		token.TOKEN_VARIABLE_END,
+		token.TOKEN_RAW_BEGIN,
+		token.TOKEN_RAW_END,
 		token.TOKEN_COMMENT_BEGIN,
-		token.TOKEN_COMMENT_END:
-		tl.segmentIndex++
-		return segment
-	case token.TOKEN_COMMENT:
+		token.TOKEN_COMMENT_END,
+		token.TOKEN_COMMENT,
+		token.TOKEN_LINESTATEMENT_BEGIN,
+		token.TOKEN_LINESTATEMENT_END,
+		token.TOKEN_LINECOMMENT_BEGIN,
+		token.TOKEN_LINECOMMENT_END,
+		token.TOKEN_LINECOMMENT:
 		tl.segmentIndex++
 		return segment
 	case token.TOKEN_STRING:
@@ -158,16 +210,6 @@ BEGIN:
 		return segment
 	}
 	panic(fmt.Sprintf("unexpected segment type: %s", segment.Type))
-}
-
-func (tl *TemplateLexer) shouldIgnoreToken(tk token.Token) bool {
-	if tk.TypeIn(tl.ignoredTokens...) {
-		return true
-	}
-	if tk.TypeIn(tl.ignoreIfEmpty...) && tk.Literal == "" {
-		return true
-	}
-	return false
 }
 
 func (tl *TemplateLexer) getPos(index int) token.Position {
@@ -196,38 +238,12 @@ func (tl *TemplateLexer) processInput() {
 	tl.positioner = token.NewLineColumnIndex(tl.input)
 }
 
-func (tl *TemplateLexer) convertToken(tk token.Token) token.Token {
-	var err error
-	switch tk.Type {
-	case token.TOKEN_DATA:
-		tk.Literal = tl.normalizeNewlines(tk.Literal)
-	case token.TOKEN_STRING:
-		if tk.Literal == "" {
-			break
-		}
-		// remote the quotes and unescape the string literal
-		literal := tk.Literal[1 : len(tk.Literal)-1]
-		// Unquote 不支持多行双引号字符串，因此分行 Unquote
-		lines := strings.Split(literal, "\n")
-		for i, line := range lines {
-			lines[i], err = strconv.Unquote(fmt.Sprintf(`"%s"`, line))
-			if err != nil {
-				tk.Type = token.TOKEN_ILLEGAL
-				tk.Literal = fmt.Sprintf("string literal unquote error: %v", err)
-				return tk
-			}
-		}
-		// 跟 jinja2 保持一致，字符串中的换行会被转换成配置中的换行序列
-		// 注意：替换的换行是文本本身的换行，而不是字符串字面量中的 \n
-		tk.Literal = strings.Join(lines, tl.config.NewlineSequence)
-	}
-	return tk
-}
-
+// splitSegments 将输入字符串分割成数据段和代码段，并存储在 tl.segments 中
 func (tl *TemplateLexer) splitSegments() {
 	index := 0
-	found := true
-	for found {
+	inRaw := false
+	rawStartIndex := -1
+	for {
 		matchCodeTag := CodeTag{}
 		matchIndex := len(tl.input) + 8
 		for _, codeTag := range tl.codeTags {
@@ -294,23 +310,7 @@ func (tl *TemplateLexer) splitSegments() {
 			Start:   tl.getPos(startIndex),
 			End:     tl.getPos(startIndex + startLen),
 		}
-		if dataToken.Literal != "" {
-			if stripSign == '-' {
-				// strip all whitespace before
-				dataToken.Literal = strings.TrimRightFunc(dataToken.Literal, unicode.IsSpace)
-			} else if stripSign != '+' && tl.config.LstripBlocks && matchCodeTag.startType != token.TOKEN_VARIABLE_BEGIN {
-				// strip whitespace from the beginning of a line to the start of a block
-				// Nothing will be stripped if there are other characters before the start of the block.
-				lastNewline := strings.LastIndex(dataToken.Literal, "\n")
-				if lastNewline != -1 {
-					if IsAllWhitespace(dataToken.Literal[lastNewline+1:]) {
-						dataToken.Literal = dataToken.Literal[:lastNewline+1]
-					}
-				} else if IsAllWhitespace(dataToken.Literal) {
-					dataToken.Literal = ""
-				}
-			}
-		}
+		tl.lstripBlocks(&dataToken, &matchCodeTag, stripSign)
 		stripSign = 0
 		endLen := len(matchCodeTag.end)
 		if tl.input[endIndex-1] == '-' || tl.input[endIndex-1] == '+' {
@@ -319,15 +319,7 @@ func (tl *TemplateLexer) splitSegments() {
 			endIndex--
 		}
 		// 行为与 jinja2 保持一致，将空白字符放在 endToken
-		if stripSign == '-' {
-			// strip all whitespace after
-			endLen += leftWhitespaceByteCount(tl.input[endIndex+endLen:])
-		} else if stripSign != '+' && tl.config.TrimBlocks && matchCodeTag.endType != token.TOKEN_VARIABLE_END {
-			// strip the first newline after a template tag
-			if strings.HasPrefix(tl.input[endIndex+endLen:], "\n") {
-				endLen++
-			}
-		}
+		endLen += tl.trimBlocks(stripSign, &matchCodeTag, endIndex+endLen)
 		endToken := token.Token{
 			Type:    matchCodeTag.endType,
 			Literal: tl.input[endIndex : endIndex+endLen],
@@ -340,10 +332,53 @@ func (tl *TemplateLexer) splitSegments() {
 			Start:   tl.getPos(startIndex + startLen),
 			End:     tl.getPos(endIndex),
 		}
-		if matchCodeTag.startType == token.TOKEN_COMMENT_BEGIN {
+		switch matchCodeTag.startType {
+		case token.TOKEN_COMMENT_BEGIN:
 			codeToken.Type = token.TOKEN_COMMENT
+		case token.TOKEN_LINECOMMENT_BEGIN:
+			codeToken.Type = token.TOKEN_LINECOMMENT
 		}
-		tl.segments = append(tl.segments, dataToken, startToken, codeToken, endToken)
+
+		if strings.TrimSpace(codeToken.Literal) == "raw" {
+			inRaw = true
+			rawStartIndex = endIndex + endLen
+			tl.segments = append(tl.segments,
+				dataToken,
+				token.Token{
+					Type:    token.TOKEN_RAW_BEGIN,
+					Literal: tl.input[startIndex : endIndex+endLen],
+					Start:   startToken.Start,
+					End:     endToken.End,
+				})
+		} else if strings.TrimSpace(codeToken.Literal) == "endraw" {
+			if !inRaw {
+				tl.segments = append(tl.segments, token.Token{
+					Type:    token.TOKEN_ILLEGAL,
+					Literal: "unexpected endraw tag",
+					Start:   startToken.Start,
+					End:     endToken.End,
+				})
+				return
+			}
+			dataToken := token.Token{
+				Type:    token.TOKEN_DATA,
+				Literal: tl.input[rawStartIndex:startIndex],
+				Start:   tl.getPos(rawStartIndex),
+				End:     tl.getPos(startIndex),
+			}
+			tl.lstripBlocks(&dataToken, &matchCodeTag, startToken.Literal[len(startToken.Literal)-1])
+			tl.segments = append(tl.segments,
+				dataToken,
+				token.Token{
+					Type:    token.TOKEN_RAW_END,
+					Literal: tl.input[startIndex : endIndex+endLen],
+					Start:   startToken.Start,
+					End:     endToken.End,
+				},
+			)
+		} else if !inRaw {
+			tl.segments = append(tl.segments, dataToken, startToken, codeToken, endToken)
+		}
 
 		index = endIndex + endLen
 	}
@@ -398,4 +433,41 @@ func (tl *TemplateLexer) isBalance(start int, end int) bool {
 		}
 	}
 	return stack.Empty()
+}
+
+// lstripBlocks 根据 stripSign 和配置决定是否需要 lstrip blocks 相关的空白字符
+// 并修改 tk.Literal (block_begin 前的 segment token)
+func (tl *TemplateLexer) lstripBlocks(tk *token.Token, codeTag *CodeTag, stripSign byte) {
+	if tk.Literal == "" {
+		return
+	}
+	if stripSign == '-' {
+		// strip all whitespace before
+		tk.Literal = strings.TrimRightFunc(tk.Literal, unicode.IsSpace)
+	} else if stripSign != '+' && tl.config.LstripBlocks && codeTag.startType != token.TOKEN_VARIABLE_BEGIN {
+		// strip whitespace from the beginning of a line to the start of a block
+		// Nothing will be stripped if there are other characters before the start of the block.
+		lastNewline := strings.LastIndex(tk.Literal, "\n")
+		if lastNewline != -1 {
+			if IsAllWhitespace(tk.Literal[lastNewline+1:]) {
+				tk.Literal = tk.Literal[:lastNewline+1]
+			}
+		} else if IsAllWhitespace(tk.Literal) {
+			tk.Literal = ""
+		}
+	}
+}
+
+// trimBlocks 根据 stripSign 和配置决定是否需要 trim blocks 相关的空白字符，并返回需要 trim 的字节数
+func (tl *TemplateLexer) trimBlocks(stripSign byte, codeTag *CodeTag, start int) int {
+	if stripSign == '-' {
+		// strip all whitespace after
+		return leftWhitespaceByteCount(tl.input[start:])
+	} else if stripSign != '+' && tl.config.TrimBlocks && codeTag.endType != token.TOKEN_VARIABLE_END {
+		// strip the first newline after a template tag
+		if strings.HasPrefix(tl.input[start:], "\n") {
+			return 1
+		}
+	}
+	return 0
 }
